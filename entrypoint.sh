5f02e76143a6cbd4a676a89ec5efe258d29a3cd9
Alejandro Gómez Montoya <agomez13@eafit.edu.co>
2023-06-23 19:03:57 -0500

    Add minio init to entrypoint.sh and remove verbose from fluentd.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
-nohup ./eventsim.sh -c "examples/example-config.json" --from 365 --nusers 1000 --growth-rate 0.01 ./output > eventsim.stoud &
-fluentd -c ./fluent.conf -vv
+mc alias set minio http://minio:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
+mc mb minio/de-streaming-test
+nohup bash eventsim.sh -c "examples/example-config.json" --from 365 --nusers 1000 --growth-rate 0.01 ./output > eventsim.stoud &
+fluentd -c ./fluent.conf
